41ebfa6db914ca06a91d1d0ab1b96e0fa07d9729
Jkapella <jvkapella@gmail.com>
2019-04-17 11:53:10 +0100

    tidied up comments in JS, added placeholder html to index

--- monsterFinder.js
+++ monsterFinder.js
@@ -12,9 +12,7 @@ function setUpButtonEventListeners() {
     })
 }
 
-
 setUpButtonEventListeners()
-
 
 //Do a request to ... http://www.dnd5eapi.co/ ... get all monsters, using http://dnd5eapi.co/api/
 //Each of these monsters has another URL to do an API call to get full info on the monster
