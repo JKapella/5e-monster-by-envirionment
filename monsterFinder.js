77511cf2800b0c857fdd2570c7c935e5309762d2
Jkapella <jvkapella@gmail.com>
2019-04-17 11:25:06 +0100

    started adding placeholder to index page

---
+++ monsterFinder.js
@@ -0,0 +1,7 @@
+//ON PAGE LOAD
+
+//GET THE
+
+
+//Do a request to ... http://www.dnd5eapi.co/ ... get all monsters, using http://dnd5eapi.co/api/
+//Each of these monsters has another URL to do an API call to get full info on the monster
